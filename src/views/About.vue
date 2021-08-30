81a14487537577dca59069f6114e90e634b6976a
costpawa <hakan_dnz_000@hotmail.com>
2021-08-30 22:00:31 +0300

    Initial commit

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,23 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-6">
+    <h1>About Vuetify Todo</h1>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id quaerat amet facilis soluta autem odio facere, a cupiditate aliquid commodi laboriosam vel sint odit! Magnam quisquam repellat ut totam officia!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut incidunt eveniet exercitationem perspiciatis quam id ullam error ipsum expedita quibusdam ea dolorum voluptatum, commodi aspernatur odio, corporis architecto inventore officia.
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id quaerat amet facilis soluta autem odio facere, a cupiditate aliquid commodi laboriosam vel sint odit! Magnam quisquam repellat ut totam officia!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut incidunt eveniet exercitationem perspiciatis quam id ullam error ipsum expedita quibusdam ea dolorum voluptatum, commodi aspernatur odio, corporis architecto inventore officia.
+    </p>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id quaerat amet facilis soluta autem odio facere, a cupiditate aliquid commodi laboriosam vel sint odit! Magnam quisquam repellat ut totam officia!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut incidunt eveniet exercitationem perspiciatis quam id ullam error ipsum expedita quibusdam ea dolorum voluptatum, commodi aspernatur odio, corporis architecto inventore officia.
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id quaerat amet facilis soluta autem odio facere, a cupiditate aliquid commodi laboriosam vel sint odit! Magnam quisquam repellat ut totam officia!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut incidunt eveniet exercitationem perspiciatis quam id ullam error ipsum expedita quibusdam ea dolorum voluptatum, commodi aspernatur odio, corporis architecto inventore officia.
+    </p>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id quaerat amet facilis soluta autem odio facere, a cupiditate aliquid commodi laboriosam vel sint odit! Magnam quisquam repellat ut totam officia!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut incidunt eveniet exercitationem perspiciatis quam id ullam error ipsum expedita quibusdam ea dolorum voluptatum, commodi aspernatur odio, corporis architecto inventore officia.
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id quaerat amet facilis soluta autem odio facere, a cupiditate aliquid commodi laboriosam vel sint odit! Magnam quisquam repellat ut totam officia!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut incidunt eveniet exercitationem perspiciatis quam id ullam error ipsum expedita quibusdam ea dolorum voluptatum, commodi aspernatur odio, corporis architecto inventore officia.
+    </p>
   </div>
 </template>
